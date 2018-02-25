2a07188febfd62c35933283206be31ba146548b1
Benji Eng <benjamin.eng7@gmail.com>
2018-02-25 12:47:10 -0800

    add shooting stars

--- stars.js
+++ stars.js
@@ -39,6 +39,32 @@ window.addEventListener("mouseup", function() {
 	isMouseDown = false;	
 });
 
+// Objects
+function ShootingStarParticle(x, y) {
+    this.x = x;
+    this.y = y;
+    this.color = 'white';
+
+    this.update = () => {
+        const lastPoint = {x: this.x, y: this.y};
+
+        //motion
+        this.x += 100;
+        this.y += 100;
+        this.draw(lastPoint)
+    }
+
+    this.draw = (lastPoint) => {
+        c.beginPath();
+        c.strokeStyle = this.color;
+        c.fillStyle = this.color;
+        c.lineWidth = .075;
+        c.moveTo(lastPoint.x, lastPoint.y); //takes particle's previous location
+        c.lineTo(this.x, this.y); //takes particles new location
+        c.stroke();
+        c.closePath()
+    }
+}
 //Star Particle object
 function StarParticle(x, y, radius, color) {
 	this.x = x;
@@ -51,22 +77,24 @@ function StarParticle(x, y, radius, color) {
 	};
 
 	this.draw = () => {
-		c.save();
-		c.beginPath();
-		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);	
-		c.shadowColor = this.color;
-		c.shadowBlur = 15;
-		c.shadowOffsetX = 0;
+		c.save(); //save current drawing state
+		c.beginPath(); //starts drawing of line
+		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);	//arcs the line
+		c.shadowColor = this.color; //color of shadow
+		c.shadowBlur = 15; //blur of shadow
+		c.shadowOffsetX = 0; //offsets of shadow
 		c.shadowOffsetY = 0;
-		c.fillStyle = this.color;
-		c.fill();
-		c.closePath();
-		c.restore();
+		c.fillStyle = this.color; //fill color of line
+		c.fill(); //fills path
+		c.closePath(); //stops the line
+		c.restore(); //restores 
 	};
 }
 
 //array of star objects
 var starParticles = [];
+//array of shooting stars
+var shootingParticles = [];
 //function to create stars
 initializeParticles = () => {
 	for (var i = 0; i < starCount; i++) {
@@ -84,22 +112,23 @@ initializeParticles = () => {
 
 var timer = 0;
 var opacity = 1;
-var speed = 0.0005;
+var speed = 0.00075;
 
 animate = () => {
+	//callback routine must itself call requestAnimationFrame() if you want to animate another frame at the next repaint.
 	window.requestAnimationFrame(animate);
 
-	c.save();
+	c.save(); //saves the drawing state
 	if (isMouseDown === true) {
 
 		// Ease into the new opacity
 		var desiredOpacity = 0.01;
-		opacity += (desiredOpacity - opacity) * 0.03;
+		opacity += (desiredOpacity - opacity) * 0.03;//
 		c.fillStyle = "rgba(18, 18, 18,"+ opacity +")";
 
 		// Ease into the new speed
-		var desiredSpeed = 0.005;
-		speed += (desiredSpeed - speed) * 0.005;
+		var desiredSpeed = 0.015;
+		speed += (desiredSpeed - speed) * 0.01;
 		timer += speed;
 
 	} else {
@@ -110,20 +139,26 @@ animate = () => {
 		c.fillStyle = "rgba(18, 18, 18, " + opacity + ")";
 
 		// Ease back to the original speed
-		var originalSpeed = 0.0005;
-		speed += (originalSpeed - speed) * 0.005;
+		var originalSpeed = 0.00075;
+		speed += (originalSpeed - speed) * 0.01;
 		timer += speed;
 
 	}
 
-	c.fillRect(0, 0, canvas.width, canvas.height);
+	
+	c.fillRect(0, 0, canvas.width, canvas.height); //fills the canvas with black color to clear star positions
 	c.translate(canvas.width / 2, canvas.height/2 );
-	c.rotate(timer);
+	c.rotate(timer); //rotates canvas by degree (use of a timer to move degrees slowly)
 
 	for (var i = 0; i < starParticles.length; i++) {
 		starParticles[i].update();
 	}
 
+	for (var i = 0; i < shootingParticles.length; i++) {
+		shootingParticles[i].update()
+	}
+
+	//restores the most recently saved canvas state by popping the top entry in the drawing state stack. 
 	c.restore();
 }
 
@@ -151,6 +186,10 @@ function distance(x1, y1, x2, y2) {
     const yDist = y2 - y1
     return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2))
 }
-
+setInterval(function(){
+	shootingParticles = [];
+	//add shooting star
+    shootingParticles.push(new ShootingStarParticle(randomIntFromRange(-300, 300), randomIntFromRange(-200, 200)));
+}, 10000);
 initializeParticles();
 animate();
